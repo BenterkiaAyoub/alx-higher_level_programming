2386d82e193287e2ce4c0ed93da6801805531cd4
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2024-01-17 05:41:38 -0800

    last comit

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- Script to list all tables of a given database
 
-/* 
+/*
    Your task description or any relevant comments can be placed here.
    For example, this script lists all tables of the specified database.
 */
@@ -8,5 +8,9 @@
 -- Make sure all SQL keywords are in uppercase, as per the requirements.
 
 USE `mysql`;
-SHOW TABLES;
+
+-- Query to retrieve all tables from the specified database
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = DATABASE();
 
