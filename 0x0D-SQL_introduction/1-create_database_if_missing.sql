bbb2872ce43fa09bfb0457a09d7a28a3c485759c
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2024-01-17 05:27:26 -0800

    first task

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,11 @@
---creates the database hbtn_0c_0
+-- Script to create database hbtn_0c_0 if it doesn't exist
+
+/* 
+   Your task description or any relevant comments can be placed here.
+   For example, this script creates the database hbtn_0c_0 if it doesn't exist.
+*/
+
+-- Make sure all SQL keywords are in uppercase, as per the requirements.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
