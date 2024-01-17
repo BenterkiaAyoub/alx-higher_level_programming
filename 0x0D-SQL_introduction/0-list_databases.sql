6aab84e68c706221b9d53c0059f37207ae17a9e2
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2024-01-17 05:26:00 -0800

    second task

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,11 @@
---lists all databases of MySQL server
+-- Script to list all databases
+
+/* 
+   Your task description or any relevant comments can be placed here.
+   For example, this script lists all databases on the MySQL server.
+*/
+
+-- Make sure all SQL keywords are in uppercase, as per the requirements.
+
 SHOW DATABASES;
+
