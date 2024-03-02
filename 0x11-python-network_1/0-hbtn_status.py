c186f9a54f560fe13b33d681404451bdd87377dd
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2024-03-02 13:11:20 -0800

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+README for 0x11-python-network_1
